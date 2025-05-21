9e5fe28ce33b44f3d2cf2d3fd4c782e47c81c92a
Ana Corrales <valentinacorrales0563@gmail.com>
2025-05-21 15:42:20 -0500

    v1.3.2

---
+++ docs/programa-about.js
@@ -0,0 +1,16 @@
+// programa-acerca-de.js
+var map = L.map('map').setView([4.7423251, -74.115069], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Aquí añadimos el polígono
+var latlngs = [
+    [4.7423251, -74.115069],
+    [4.7435000, -74.114000],
+    [4.7450000, -74.113500]
+];
+var polygon = L.polygon(latlngs).addTo(map);
+map.fitBounds(polygon.getBounds());  // Ajustar el mapa para mostrar todo el polígono
