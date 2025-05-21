b22de90524984b7fa663b59738de0d511695c76f
Ana Corrales <valentinacorrales0563@gmail.com>
2025-05-21 15:58:08 -0500

    v1.3.3

--- docs/programa-about.js
+++ docs/programa-about.js
@@ -1,16 +1,20 @@
 // programa-acerca-de.js
-var map = L.map('map').setView([4.7423251, -74.115069], 13);
+var map = L.map('map').setView([4.7423251, -74.115069], 13); // Coordenadas iniciales del mapa
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Aquí añadimos el polígono
+// Coordenadas del polígono (verifica que estén dentro de un área visible)
 var latlngs = [
     [4.7423251, -74.115069],
     [4.7435000, -74.114000],
     [4.7450000, -74.113500]
 ];
+
+// Crear el polígono y agregarlo al mapa
 var polygon = L.polygon(latlngs).addTo(map);
-map.fitBounds(polygon.getBounds());  // Ajustar el mapa para mostrar todo el polígono
+
+// Ajustar el mapa para mostrar todo el polígono
+map.fitBounds(polygon.getBounds());
